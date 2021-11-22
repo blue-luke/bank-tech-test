9f163c04f9b89e3c47fd7094cc05453efe902996
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-11-22 16:31:03 +0000

    Second test done, stil to refactor

--- __tests__/account.test.js
+++ __tests__/account.test.js
@@ -5,9 +5,19 @@ const Account = require('../account')
 const t1 = '10/01/2023:1000.00:0.00'
 
 // returning data in defined format
-const r1 = 'date || credit || debit || balance \n10/01/2023 || 1000.00 || 0.00 || 1000.00'
+const r1 = 'date || credit || debit || balance\n10/01/2023 || 1000.00 || 0.00 || 1000.00'
 
 test('entering t1 returns y1', () => {
   account = new Account()
-  expect(account.transaction(t1)).toBe(r1)
+  account.transaction(t1)
+  expect(account.print_statement()).toBe(r1)
+});
+
+const t2 = '13/01/2023:2000.00:0.00'
+
+const r2 = 'date || credit || debit || balance\n13/01/2023 || 2000.00 || 0.00 || 3000.00\n10/01/2023 || 1000.00 || 0.00 || 1000.00'
+
+test('entering t1 returns y1', () => {
+  account.transaction(t2)
+  expect(account.print_statement()).toBe(r2)
 });
--- account.js
+++ account.js
@@ -1,12 +1,26 @@
 class Account {
+
+  constructor() {
+    this.balance = 0.00
+    this.transaction_record = 'date || credit || debit || balance'
+  }
+
   transaction(input_data) {
     const output_data = input_data.split(':')
     const date = output_data[0]
-    const debit_amount = output_data[1]
-    const credit_amount = output_data[2]
-    const column_headings = 'date || credit || debit || balance \n'
-    `${column_headings}${date} || ${debit_amount} || ${credit_amount} || 1000.00`
+    const credit_amount = output_data[1]
+    const debit_amount = output_data[2]
+    this.balance = this.balance + parseInt(credit_amount) - parseInt(debit_amount)
+    // this.transaction_record = this.transaction_record.concat(`\n${date} || ${credit_amount} || ${debit_amount} || 1000.00`)
+    // this.transaction_record = this.transaction_record.splice(34, 0, `\n${date} || ${credit_amount} || ${debit_amount} || ${this.balance}`)
+    this.transaction_record = [this.transaction_record.slice(0, 34), `\n${date} || ${credit_amount} || ${debit_amount} || ${this.balance.toFixed(2)}`, this.transaction_record.slice(34)].join('')
   }
+
+
+  print_statement() {
+    return this.transaction_record
+  }
+
 }
 
 module.exports = Account
