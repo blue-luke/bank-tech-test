239f9ba8209af0599a32846960291d16bd7abecb
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-11-22 17:27:15 +0000

    3rd test and core functionality complete. Writing readme

--- __tests__/account.test.js
+++ __tests__/account.test.js
@@ -1,23 +1,30 @@
 const Account = require('../account')
 
 // data entered as 'DD/MM/YYY:[credit]:[debit]'
-// transaction 1
+// t1 = transaction 1
 const t1 = '10/01/2023:1000.00:0.00'
-
 // returning data in defined format
-const r1 = 'date || credit || debit || balance\n10/01/2023 || 1000.00 || 0.00 || 1000.00'
+// r1 = return value 1
+const r1 = 'date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00'
 
-test('entering t1 returns y1', () => {
+test('entering t1 returns r1', () => {
   account = new Account()
   account.transaction(t1)
   expect(account.print_statement()).toBe(r1)
 });
 
 const t2 = '13/01/2023:2000.00:0.00'
+const r2 = 'date || credit || debit || balance\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00'
 
-const r2 = 'date || credit || debit || balance\n13/01/2023 || 2000.00 || 0.00 || 3000.00\n10/01/2023 || 1000.00 || 0.00 || 1000.00'
-
-test('entering t1 returns y1', () => {
+test('entering t1 and t2 returns r2', () => {
   account.transaction(t2)
   expect(account.print_statement()).toBe(r2)
+});
+
+const t3 = '14/01/2023:0.00:500.00'
+const r3 = 'date || credit || debit || balance\n14/01/2023 || || 500.00 || 2500.00\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00'
+
+test('entering t1 and t2 and t3 returns r3', () => {
+  account.transaction(t3)
+  expect(account.print_statement()).toBe(r3)
 });
--- account.js
+++ account.js
@@ -6,16 +6,40 @@ class Account {
   }
 
   transaction(input_data) {
+    // extract and name data from the raw string input data
     const output_data = input_data.split(':')
     const date = output_data[0]
-    const credit_amount = output_data[1]
-    const debit_amount = output_data[2]
-    this.balance = this.balance + parseInt(credit_amount) - parseInt(debit_amount)
-    // this.transaction_record = this.transaction_record.concat(`\n${date} || ${credit_amount} || ${debit_amount} || 1000.00`)
-    // this.transaction_record = this.transaction_record.splice(34, 0, `\n${date} || ${credit_amount} || ${debit_amount} || ${this.balance}`)
-    this.transaction_record = [this.transaction_record.slice(0, 34), `\n${date} || ${credit_amount} || ${debit_amount} || ${this.balance.toFixed(2)}`, this.transaction_record.slice(34)].join('')
+    var credit_amount = parseInt(output_data[1])
+    var debit_amount = parseInt(output_data[2])
+
+    this.update_balance(credit_amount, debit_amount)
+    this.update_transaction_record(date, credit_amount, debit_amount)
   }
 
+  update_balance(credit_amount, debit_amount) {
+    this.balance = this.balance + credit_amount - debit_amount
+  }
+
+  update_transaction_record(date, credit_amount, debit_amount) {
+    // update transaction record
+    // complicated because 0.00 credit needs to be represented as an empty string when displayed, not as 0.00
+    if (credit_amount == 0.00) {
+      credit_amount = ''
+    } else {
+      credit_amount = String(credit_amount.toFixed(2)) + ' '
+    }
+
+    if (debit_amount == 0.00) {
+      debit_amount = ''
+    } else {
+      debit_amount = String(debit_amount.toFixed(2)) + ' '
+    }
+
+    const new_transaction_line = `\n${date} || ${credit_amount}|| ${debit_amount}|| ${this.balance.toFixed(2)}`
+
+    // insert the new transaction line after the column headings at index position 34 but before older transactions after index position 34
+    this.transaction_record = [this.transaction_record.slice(0, 34), new_transaction_line, this.transaction_record.slice(34)].join('')
+  }
 
   print_statement() {
     return this.transaction_record
