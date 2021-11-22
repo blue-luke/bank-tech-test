5c07789ef52fdb6ae451468d58b035662bdb75a8
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-11-22 15:25:46 +0000

    First test refactored

--- account.js
+++ account.js
@@ -1,7 +1,11 @@
 class Account {
   transaction(input_data) {
     const output_data = input_data.split(':')
-    return `date || credit || debit || balance \n${output_data[0]} || ${output_data[1]} || ${output_data[2]} || 1000.00`
+    const date = output_data[0]
+    const debit_amount = output_data[1]
+    const credit_amount = output_data[2]
+    const column_headings = 'date || credit || debit || balance \n'
+    `${column_headings}${date} || ${debit_amount} || ${credit_amount} || 1000.00`
   }
 }
 
